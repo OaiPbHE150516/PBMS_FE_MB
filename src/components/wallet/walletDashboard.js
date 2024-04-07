8eeb47afdf9c871d94cf618a814c00f2bfdb1db8
BaOai.Work <oaipbhe150516@fpt.edu.vn>
2024-04-08 00:46:26 +0700

    Update walletDashboard.js

--- src/components/wallet/walletDashboard.js
+++ src/components/wallet/walletDashboard.js
@@ -11,51 +11,61 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import pbms from "../../api/pbms";
 import { API } from "../../constants/api.constant";
+import { fetchAllData } from "../../redux/dataSlice";
+
+// services
+import walletServices from "../../services/walletServices";
 
 const WalletDashboard = () => {
   const account = useSelector((state) => state.authen.account);
+  const shouldFetchData = useSelector((state) => state.data.shouldFetchData);
   const [eachWallet, setEachWallet] = useState({});
-  const fetchData = async (accountID) => {
+
+  async function fetchWalletData(accountID) {
+    console.log("fetchWalletData accountID: ", accountID);
     try {
-      const eachWallet = await pbms.get(
-        API.WALLET.GET_EACH_WALLET_BALANCE + accountID
-      );
-      setEachWallet(eachWallet.data);
+      await walletServices
+        .getTotalBalanceEachWallet(accountID)
+        .then((response) => {
+          // set EachWallet is top 2 wallet of response
+          setEachWallet(response.slice(0, 2));
+        });
     } catch (error) {
-      console.error("Error fetching wallet data:", error);
+      console.error("Error fetchWalletData data:", error);
     }
-  };
+  }
 
   useEffect(() => {
-    if (account !== null) {
-      fetchData(account.accountID);
-    }
-  }, [account]);
+    fetchWalletData(account.accountID);
+  }, [shouldFetchData]);
 
-  return eachWallet ? (
-    <View style={styles.viewStyle}>
-      <FlatList
-        data={eachWallet}
-        keyExtractor={(item) => item.walletID}
-        renderItem={({ item }) => {
-          return (
-            <View style={styles.walletItem}>
-              <Text style={styles.name}>{item.name}</Text>
-              <Text style={styles.balance}>{item.balance}</Text>
-            </View>
-          );
-        }}
-      />
-    </View>
-  ) : null;
+  return (
+    eachWallet && (
+      <View style={styles.viewStyle}>
+        <FlatList
+          data={eachWallet}
+          keyExtractor={(item) => item.walletID}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.walletItem}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text style={styles.balance}>{item.balance}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
+    )
+  );
 };
 const styles = StyleSheet.create({
   viewStyle: {
     borderBlockColor: "dimgray",
     flex: 1,
-    borderWidth: 1,
+    borderWidth: 0.5,
     justifyContent: "space-between",
     alignItems: "center",
+    alignContent: "center",
     flexDirection: "row",
     marginTop: 0,
     borderRadius: 5,
@@ -69,7 +79,9 @@ const styles = StyleSheet.create({
     padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: "lightgrey",
-    width: "100%"
+    width: "98%",
+    flex: 1,
+    marginHorizontal: 2
   },
 
   name: {
